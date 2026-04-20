184f3095faafc196d52a2fb7f208fbda267c8284
devgabrielcoman <dev.gabriel.coman@gmail.com>
2026-04-20 11:29:13 +0100

    added more sections

--- docs-src/Index.tsx
+++ docs-src/Index.tsx
@@ -1,10 +1,12 @@
+import { PRODUCT_NAME, REACT_COMPONENTS_URL, REACT_PROPS_URL, REACT_URL } from "./constants";
+
 export default function Index() {
   return <>
     <header>
       <div className="hero align-center">
-        <h1>React Srv</h1>
+        <h1>{PRODUCT_NAME}</h1>
         <p>
-          React Srv is a small project that allows you to very quickly serve React pages from an existing route.
+          {PRODUCT_NAME} is a small project that allows you to very quickly serve React pages from an existing route.
         </p>
         <p>
           <a href="https://github.com/gobi-tools/react-srv" target="_blank">
@@ -18,6 +20,7 @@ export default function Index() {
       </div>
     </header>
     <main>
+      {/* simplest demo */}
       <section>
         <p>
           All you need to do is define a React component in a .tsx file:
@@ -26,13 +29,12 @@ export default function Index() {
           <pre><code>{`export default function Page() {
   return <>
     <h1>Hello, world!</h1>
-    <p>Demo time</p>
   </>
 }`}</code></pre>
           <figcaption>page.tsx</figcaption>
         </figure>
         <p>
-          And React Srv will render it as static HTML you can send down the wire:
+          And {PRODUCT_NAME} will render it as static HTML you can send down the wire:
         </p>
         <figure>
           <pre><code>{`const app = express();
@@ -42,14 +44,15 @@ app.get('/', (_, res) => {
   return res.status(200).send(react.render(Page, {}));
 });
 `}</code></pre>
-          <figcaption>server.tsx</figcaption>
+          <figcaption>server.ts</figcaption>
         </figure>
       </section>
 
+      {/* install */ }
       <section>
         <h2>Setup</h2>
         <p>
-          First, install the latest versions of the <code>react-srv</code> library and <a href="https://react.dev/" target="_blank">React</a>:
+          First, install the latest versions of the <code>react-srv</code> library and <a href={REACT_URL} target="_blank">React</a>:
         </p>
         <figure>
           <pre><code>{`npm i react-srv
@@ -74,6 +77,109 @@ npm i @types/react-dom@19.2.0 --save-dev`}</code></pre>
 }`}</code></pre>
           <figcaption>tsconfig.json</figcaption>
         </figcaption>
+      </section>
+
+      {/* documents */}
+      <section>
+        <h2>Documents</h2>
+        <p>
+          {PRODUCT_NAME} will wrap all components in a default HTML document. You may create a custom one ...
+        </p>
+        <figure>
+          <pre><code>{`export default function Document({ children }) {
+  return <html lang="en">
+    <head>
+      <title>My document</title>
+      {/*... all other meta tags, link tags, etc */
+    </head>
+    <body>
+      {children}
+    </body>
+  </html>
+}`}</code></pre>
+          <figcaption>Document.tsx</figcaption>
+        </figure>
+        <p>
+          ... which you can reference when creating the <code>ReactSrv</code> instance:
+        </p>
+        <figure>
+          <pre><code>{`import Document from "./Document";
+
+const react = new ReactSrv({ Document });`}</code></pre>
+          <figcaption>server.ts</figcaption>
+        </figure>
+        <p>
+          In this way you  can even have different documents for different routes, if you wish.
+        </p>
+      </section>
+
+      {/* components */}
+      <section>
+        <h2>Components</h2>
+        <p>
+          Just like in <a href={REACT_COMPONENTS_URL} target="_blanl">any React app</a>, 
+          you can split a big page into multiple components.
+        </p>
+        <figure>
+          <pre><code>{`function MyButton() { 
+  return <button>Click me!</button>
+}
+
+export default function Page() {
+  return <>
+    <h1>Hello, world!</h1>
+    <MyButton/>
+  </>
+}`}</code></pre>
+          <figcaption>Page.tsx</figcaption>
+        </figure>
+      </section>
+
+      {/* props */}
+      <section>
+        <h2>Props</h2>
+        <p>
+          Pages and components don't need to be static. You can define any <a href={REACT_PROPS_URL} target="_blank">props</a> ...
+        </p>
+        <figure>
+          <pre><code>{`export default function Page(props: { name: string }) {
+  return <>
+    <h1>Hello, {props.name}!</h1>
+    <MyButton/>
+  </>
+}`}</code></pre>
+          <figcaption>Page.tsx</figcaption>
+        </figure>
+        <p>
+          ... and pass them to the rendering function. 
+        </p>
+        <figure>
+          <pre><code>{`app.get('/', (req, res) => {
+  const name = req.query['name'];
+  return res.status(200).send(react.render(Page, { name }));
+});`}</code></pre>
+          <figcaption>server.ts</figcaption>
+        </figure>
+      </section>
+
+      {/* internal state & serving dynamic content  */}
+      <section>
+        <h2>State</h2>
+      </section>
+
+      {/* zustand */}
+      <section>
+        <h2>Zustand</h2>
+      </section>
+
+      {/* serving cached react */}
+      <section>
+        <h2>Cache</h2>
+      </section>
+      
+      {/* static websites */}
+      <section>
+        <h2>Static websites</h2>
       </section>
     </main>
   </>
